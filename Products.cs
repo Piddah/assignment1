36d90fec9c286de3050f391bc64a5649fdf190ee
Peter Andrén <piddah80@gmail.com>
2024-11-19 13:43:20 +0100

    First Commit

---
+++ Products.cs
@@ -0,0 +1,9 @@
+namespace Assignment;
+
+public record class Products
+(
+    string ProductName,
+    double Vat
+);
+
+
---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using Assignment;
+bool wrongProductName = true;
+
+List<Products> products =
+[
+    new( "music", 0.06 ),
+    new( "food", 0.12 ),
+    new( "alcohol", 0.25 )
+];
+
+
+double Purchase(string product, double price)
+{
+
+    foreach (var p in products)
+    {
+        if (product.ToLower().Equals(p.ProductName))
+        {
+            wrongProductName = false;
+            return price + (price * p.Vat);
+        }
+    }
+
+    throw new ArgumentException("Product isnt in the list");
+}
+
